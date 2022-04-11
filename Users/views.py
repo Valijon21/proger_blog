935f5f6ffe4237cb4208398de6cc9b2c1b5dcd02
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-04-11 17:12:01 +0500

    add form mesage

--- Users/views.py
+++ Users/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def register(request):
-    form = UserCreationForm()
-    return  render(request, 'users/registraion.html',{'from':form, 'title':"foydalanuvchilarni ruyhatga olish"})
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'foydalanuvchi {username} ruyhatdan utdi')
+            return redirect('blog-home')
+    else:  
+        form = UserCreationForm()
+    return  render(request, 'users/registraion.html',{'form':form, 'title':"Ruyhatga olish"})
--- proger/urls.py
+++ proger/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from Users import views as userViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('reg/',userViews.register, name='reg'),
     path('',include('blog.urls')),
 ]
