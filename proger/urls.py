a3fc190d3bc99fa2d7d942df30f1d9477b7ddfb1
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-04-12 14:19:08 +0500

    add login

--- proger/urls.py
+++ proger/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from Users import views as userViews
-
+from django.contrib.auth import views as authViews
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('reg/',userViews.register, name='reg'),
+    path('auth/',authViews.LoginView.as_view(template_name='users/auth.html'), name='auth'),
+    path('exit/',authViews.LogoutView.as_view(template_name='users/exit.html'), name='exit'),
     path('',include('blog.urls')),
 ]
