dcfd18de4ed8de9c7f5000d0812ff6a089aec38a
Valijon21 <74312398+Valijon21@users.noreply.github.com>
2022-04-12 12:12:50 +0500

    add froms

--- Users/views.py
+++ Users/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
 from django.contrib import messages
+from .forms import Userreg
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = Userreg(request.POST)
         if form.is_valid():
+            form.save() #formadagi malumotlarni bazaga saqlash
             username = form.cleaned_data.get('username')
             messages.success(request, f'foydalanuvchi {username} ruyhatdan utdi')
             return redirect('blog-home')
     else:  
-        form = UserCreationForm()
+        form = Userreg()
     return  render(request, 'users/registraion.html',{'form':form, 'title':"Ruyhatga olish"})
